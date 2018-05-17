a2ae7c1f3c6cd15ca6a1c0502a027202d3d8af87
Larmik <36506614+Larmik@users.noreply.github.com>
2018-05-17 21:01:10 +0200

    Revert "page login terminée"

--- app/src/main/java/fr/wcs/laboutiquedewatto/ConnexionActivity.java
+++ app/src/main/java/fr/wcs/laboutiquedewatto/ConnexionActivity.java
@@ -25,16 +25,28 @@ public class ConnexionActivity extends AppCompatActivity {
         final EditText editTextPseudo = findViewById(R.id.et_pseudo);
         final EditText editTextPassword = findViewById(R.id.et_password);
         final EditText editTextPassword2 = findViewById(R.id.et_password2);
-
+        final ImageView imageViewPassword = findViewById(R.id.iv_password_connection);
         final ImageView imageViewPassword2 = findViewById(R.id.iv_password_connection2);
         final ImageView imageViewLigne = findViewById(R.id.iv_ligne);
         final ImageView imageViewLigneBis = findViewById(R.id.iv_ligne_bis);
         final Button buttonConnexion = findViewById(R.id.b_log_in);
         final Button buttonMember = findViewById(R.id.b_create);
         final TextView textViewOr = findViewById(R.id.tv_or);
+        final TextView textViewForgottenPassword = findViewById(R.id.tv_forgotten_password);
 
+        imageViewPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
+                if (mPasswordVisibility == PASSWORD_HIDDEN) {
+                    editTextPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    mPasswordVisibility = PASSWORD_VISIBLE;
+                } else {
+                    editTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    mPasswordVisibility = PASSWORD_HIDDEN;
+                }
+            }
+        });
 
         imageViewPassword2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,7 +81,7 @@ public class ConnexionActivity extends AppCompatActivity {
                 imageViewLigne.setVisibility(View.GONE);
                 imageViewLigneBis.setVisibility(View.GONE);
                 textViewOr.setVisibility(View.GONE);
-
+                textViewForgottenPassword.setVisibility(View.GONE);
             }
         });
     }
