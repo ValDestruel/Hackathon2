847e41bad4eee4b8e79b228913e7246823ac82f6
Valentin Destruel <destruel.valentin@gmail.com>
2018-05-17 15:50:52 +0200

    Button menu

--- app/src/main/java/fr/wcs/laboutiquedewatto/MainActivity.java
+++ app/src/main/java/fr/wcs/laboutiquedewatto/MainActivity.java
@@ -1,12 +1,22 @@
 package fr.wcs.laboutiquedewatto;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 import java.util.ArrayList;
 
@@ -15,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> al;
     private ArrayAdapter<String> arrayAdapter;
     private int i;
+    int mStratAngle = 180;
+    int mEndAngle = 270;
+    FloatingActionMenu mActionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,5 +96,72 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        /** Partie menu Circle**/
+        //Image bouton Menu
+        ImageView iconMenu = new ImageView(this);
+        iconMenu.setImageDrawable(ContextCompat.getDrawable(getApplication(), R.drawable.button_action_dark_touch));
+
+        //creation bouton Menu
+        final FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+                .setContentView(iconMenu)
+                .build();
+
+        SubActionButton.Builder listeBuilder = new SubActionButton.Builder(this);
+
+        final ImageView ivFiltre = new ImageView(this);
+        ivFiltre.setImageDrawable(ContextCompat.getDrawable(getApplication(), R.drawable.icon_password));
+        final SubActionButton filtre = listeBuilder.setContentView(ivFiltre).build();
+
+        final ImageView ivListe = new ImageView(this);
+        ivListe.setImageDrawable(ContextCompat.getDrawable(getApplication(), R.drawable.ligne));
+        final SubActionButton liste = listeBuilder.setContentView(ivListe).build();
+
+        final ImageView ivArgent = new ImageView(this);
+        ivArgent.setImageDrawable(ContextCompat.getDrawable(getApplication(), R.drawable.button_sub_action));
+        final SubActionButton argent = listeBuilder.setContentView(ivArgent).build();
+
+        Resources ressource = getResources();
+        int valuePx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, ressource.getDisplayMetrics());
+
+        DrawerLayout.LayoutParams layoutParam = new DrawerLayout.LayoutParams(valuePx, valuePx);
+        filtre.setLayoutParams(layoutParam);
+        liste.setLayoutParams(layoutParam);
+        argent.setLayoutParams(layoutParam);
+
+        mActionMenu = new FloatingActionMenu.Builder(MainActivity.this)
+                .addSubActionView(filtre)
+                .addSubActionView(liste)
+                .addSubActionView(argent)
+                .setStartAngle(mStratAngle)
+                .setEndAngle(mEndAngle)
+                .attachTo(actionButton)
+                .build();
+
+        ivFiltre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentProfile = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intentProfile);
+            }
+        });
+
+        ivListe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentListe = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intentListe);
+            }
+        });
+
+        ivArgent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentArgent = new Intent(MainActivity.this, ArgentActivity.class);
+                startActivity(intentArgent);
+            }
+        });
     }
 }
